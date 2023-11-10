bcd4e748979e445c1f8af106874742854056056b
Mammmth <2571653402@qq.com>
2023-11-10 15:57:41 +0800

    计数器装饰器代码修改

--- wrapper/ClassLoggerWrapper.py
+++ wrapper/ClassLoggerWrapper.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import Logging
 from Logging import MyLogger
 
@@ -8,11 +10,15 @@ def ClassLoggerWrapper(obj):
     def time_logger_wrapper(self, print_logger: bool = False):
         def decorator(func):
             def wrapper(*args, **kwargs):
+                start_time = datetime.now()
                 if print_logger:
-                    logger.out(Logging.DEBUG, f"Function: {func.__name__}, Args: {args}, kwargs: {kwargs}")
+                    logger.out(Logging.DEBUG, f"Function: {func.__name__}, Args: {args}, kwargs: {kwargs}, StartTime: {start_time.strftime('%Y-%m-%d %H:%M:%S')}")
                 res = func(*args, **kwargs)
+                end_time = datetime.now()
+                sub_time = end_time - start_time
                 if print_logger:
-                    logger.out(Logging.DEBUG, f"Function: {func.__name__}, Args: {args}, kwargs: {kwargs}")
+                    logger.out(Logging.DEBUG, f"Function: {func.__name__}, Args: {args}, kwargs: {kwargs}, EndTime: {end_time.strftime('%Y-%m-%d %H:%M:%S')}")
+                    logger.out(Logging.DEBUG,f"Function: {func.__name__}, Args: {args}, kwargs: {kwargs}, ExecuteTime: {str(sub_time.total_seconds())}s")
                 return res
 
             return wrapper
@@ -22,11 +28,18 @@ def ClassLoggerWrapper(obj):
     def async_time_logger_wrapper(self, print_logger: bool = False):
         def decorator(func):
             async def wrapper(*args, **kwargs):
+                start_time = datetime.now()
                 if print_logger:
-                    logger.out(Logging.DEBUG, f"func: {func.__name__} args: {args} kwargs: {kwargs}")
+                    logger.out(Logging.DEBUG,
+                               f"Function: {func.__name__}, Args: {args}, kwargs: {kwargs}, StartTime: {start_time.strftime('%Y-%m-%d %H:%M:%S')}")
                 res = await func(*args, **kwargs)
+                end_time = datetime.now()
+                sub_time = end_time - start_time
                 if print_logger:
-                    logger.out(Logging.DEBUG, f"func: {func.__name__} args: {args} kwargs: {kwargs}")
+                    logger.out(Logging.DEBUG,
+                               f"Function: {func.__name__}, Args: {args}, kwargs: {kwargs}, EndTime: {end_time.strftime('%Y-%m-%d %H:%M:%S')}")
+                    logger.out(Logging.DEBUG,
+                               f"Function: {func.__name__}, Args: {args}, kwargs: {kwargs}, ExecuteTime: {str(sub_time.total_seconds())}s")
                 return res
 
             return wrapper
--- wrapper/LoggerWrapper.py
+++ wrapper/LoggerWrapper.py
@@ -1,17 +1,26 @@
+from datetime import datetime
+
 import Logging
 from Logging import MyLogger
 
 logger = MyLogger()
 
 
-def time_logger_wrapper(self, print_logger: bool = False):
+def time_logger_wrapper(print_logger: bool = False):
     def decorator(func):
         def wrapper(*args, **kwargs):
+            start_time = datetime.now()
             if print_logger:
-                logger.out(Logging.DEBUG, f"Function: {func.__name__}, Args: {args}, kwargs: {kwargs}")
+                logger.out(Logging.DEBUG,
+                           f"Function: {func.__name__}, Args: {args}, kwargs: {kwargs}, StartTime: {start_time.strftime('%Y-%m-%d %H:%M:%S')}")
             res = func(*args, **kwargs)
+            end_time = datetime.now()
+            sub_time = end_time - start_time
             if print_logger:
-                logger.out(Logging.DEBUG, f"Function: {func.__name__}, Args: {args}, kwargs: {kwargs}")
+                logger.out(Logging.DEBUG,
+                           f"Function: {func.__name__}, Args: {args}, kwargs: {kwargs}, EndTime: {end_time.strftime('%Y-%m-%d %H:%M:%S')}")
+                logger.out(Logging.DEBUG,
+                           f"Function: {func.__name__}, Args: {args}, kwargs: {kwargs}, ExecuteTime: {str(sub_time.total_seconds())}s")
             return res
 
         return wrapper
@@ -19,14 +28,21 @@ def time_logger_wrapper(self, print_logger: bool = False):
     return decorator
 
 
-def async_time_logger_wrapper(self, print_logger: bool = False):
+def async_time_logger_wrapper(print_logger: bool = False):
     def decorator(func):
         async def wrapper(*args, **kwargs):
+            start_time = datetime.now()
             if print_logger:
-                logger.out(Logging.DEBUG, f"func: {func.__name__} args: {args} kwargs: {kwargs}")
+                logger.out(Logging.DEBUG,
+                           f"Function: {func.__name__}, Args: {args}, kwargs: {kwargs}, StartTime: {start_time.strftime('%Y-%m-%d %H:%M:%S')}")
             res = await func(*args, **kwargs)
+            end_time = datetime.now()
+            sub_time = end_time - start_time
             if print_logger:
-                logger.out(Logging.DEBUG, f"func: {func.__name__} args: {args} kwargs: {kwargs}")
+                logger.out(Logging.DEBUG,
+                           f"Function: {func.__name__}, Args: {args}, kwargs: {kwargs}, EndTime: {end_time.strftime('%Y-%m-%d %H:%M:%S')}")
+                logger.out(Logging.DEBUG,
+                           f"Function: {func.__name__}, Args: {args}, kwargs: {kwargs}, ExecuteTime: {str(sub_time.total_seconds())}s")
             return res
 
         return wrapper
