5ca1ec9f64ceb157095ed490fa66a76aa502e63b
omoidesu <949395971@qq.com>
2023-11-14 23:22:02 +0800

    feat: 更新example

--- dodo/example/botTest.py
+++ dodo/example/botTest.py
@@ -1,13 +1,22 @@
+import json
+
 from dodo import Bot
 from dodo.interface.message import Message
 
+with open("bot.json", "r", encoding="utf-8") as f:
+    bot_info = json.load(f)
+
 # 初始化bot
-bot = Bot("bot_id", "bot_token")
+bot = Bot(bot_info.get("client_id"), bot_info.get("token"))
+
 
 # 在这里你只需要专注你的业务而无需考虑和Dodo通信的问题
-@bot.on_message("info", prefix=['/', '.', ','])
-async def func1(msg: Message):
+@bot.on_message("ping", prefix=['/', '.', ','])
+async def ping(msg: Message):
     print("我是业务方法")
+    reply_message: Message = await msg.reply("pong!")
+    await reply_message.add_reaction("👍")
+
 
 # bot的运行方法
 if __name__ == '__main__':
