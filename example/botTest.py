60bc18fb0c98343ee4a5c3ee5ab5ae7e654d684a
Mammmth <2571653402@qq.com>
2023-12-04 13:09:37 +0800

    refactor: example

--- example/botTest.py
+++ example/botTest.py
@@ -1,5 +1,4 @@
 import json
-import time
 
 from dodo import Bot
 from dodo.eventMessage.msg import Msg
@@ -11,26 +10,26 @@ with open("bot.json", "r", encoding="utf-8") as f:
 bot = Bot(bot_info.get("client_id"), bot_info.get("token"))
 
 
-# 在这里你只需要专注你的业务而无需考虑和Dodo通信的问题
-@bot.on_message("ping", prefix=['/', '.', ','], at_bot=False)
+# 这里开始你的业务逻辑，服务器中输入.ping 或 /ping即可跳转至对应方法
+@bot.on_message("ping", prefix=['/', '.'], at_bot=False)
 async def ping(msg: Msg):
-    # print("我是业务方法")
-    # at成员id
-    # print(msg.body.content_info.__dict__())
+    """
+    业务主体
+    """
     # 回复消息
     reply_message: Msg = await msg.reply("pong!")
     # 给回复的消息添加回应
     await reply_message.add_reaction("👍")
-    time.sleep(2)
+    # 移除表情
     await reply_message.remove_reaction("👍")
-    # time.sleep(2)
-    # await reply_message.edit("我现在不是pong了")
-    # time.sleep(2)
-    # await reply_message.delete("我撤回了略略略")
-    # time.sleep(2)
-    # await reply_message.top()
-    # time.sleep(2)
-    # await reply_message.cancel_top()
+    # 修改消息
+    await reply_message.edit("我现在不是pong了")
+    # 置顶消息
+    await reply_message.top()
+    # 取消置顶
+    await reply_message.cancel_top()
+    # 撤回消息
+    await reply_message.delete("我撤回了略略略")
 
 
 # bot的运行方法
